8e0825a5eef01208f1f88a4b4dff51aa1fc1602a
SypherKx <itskaranpratapsingh@gmail.com>
2025-12-06 15:57:50 +0530

    fix: FULLY remove api folder

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add the current directory to sys.path so we can import from 'backend'
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from backend.main import app
